a0d1e0fc6f5d84d1202ea5506f2ab5e3121e66e9
JulienBara <bara.julien@yahoo.fr>
2016-11-19 18:49:52 +0000

    update input format

--- parse.py
+++ parse.py
@@ -9,42 +9,42 @@ def parse(conn, data, host, port):
 
     lines = data.split("\n")
 
-    if lines[0][:15] == "JOIN_CHATROOM: ":
+    if lines[0][:14] == "JOIN_CHATROOM:":
 
         print "Now in case join_chatroom"
 
-        chatroom_name = lines[0][15:]
-        client_ip = lines[1][11:]
-        client_port = lines[2][6:]
-        client_name = lines[3][13:]
+        chatroom_name = lines[0][14:]
+        client_ip = lines[1][10:]
+        client_port = lines[2][5:]
+        client_name = lines[3][12:]
         joining(conn, chatroom_name, client_ip, client_port, client_name, host, port)
 
-    elif lines[0][:16] == "LEAVE_CHATROOM: ":
+    elif lines[0][:15] == "LEAVE_CHATROOM:":
 
         print "Now in case leave_chatroom"
 
-        chatroom_id = lines[0][16:]
-        join_id = lines[1][9:]
-        client_name = lines[2][13:]
+        chatroom_id = lines[0][15:]
+        join_id = lines[1][8:]
+        client_name = lines[2][12:]
         leaving(conn, chatroom_id, join_id, client_name)
 
-    elif lines[0][:12] == "DISCONNECT: ":
+    elif lines[0][:11] == "DISCONNECT:":
 
         print "Now in case disconnect"
 
-        client_ip = lines[0][12:]
-        client_port = lines[1][6:]
-        client_name = lines[2][13:]
+        client_ip = lines[0][11:]
+        client_port = lines[1][5:]
+        client_name = lines[2][12:]
         disconnect(conn, client_ip, client_port, client_name)
 
-    elif lines[0][:6] == "CHAT: ":
+    elif lines[0][:5] == "CHAT:":
 
         print "Now in case chat"
 
-        chatroom_id = lines[0][6:]
-        join_id = lines[1][9:]
-        client_name = lines[2][13:]
-        message = lines[3][9:]
+        chatroom_id = lines[0][5:]
+        join_id = lines[1][8:]
+        client_name = lines[2][12:]
+        message = lines[3][8:]
         messaging(conn, chatroom_id, join_id, client_name, message)
 
     else:
