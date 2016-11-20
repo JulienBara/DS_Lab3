35ff49f0638562afd51679ba62b575f2ccc634e6
JulienBara <bara.julien@yahoo.fr>
2016-11-20 17:50:42 +0000

    test

--- server.py
+++ server.py
@@ -61,6 +61,8 @@ def killsHandler(_sign, _stack_frame):
     global conns
     global s
 
+    os.kill(os.getppid(), signal.SIGTERM)
+
     for conn in conns:
         # conn.close()
         conn.shutdown(socket.SHUT_RDWR)
