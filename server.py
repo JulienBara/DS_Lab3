adef2b863d48751e5f7e5feaf29838811beaf325
JulienBara <bara.julien@yahoo.fr>
2016-11-20 16:09:21 +0000

    Revert "const"
    This reverts commit e662d523ae11f69752081d05d562ceabef417442.

--- server.py
+++ server.py
@@ -59,7 +59,7 @@ def clientThread(conn):
             data = conn.recv(4096)
         except:
             print "socket timeout"
-            time.sleep(1)
+            time.sleep(10)
             pass
             # break
 
@@ -109,7 +109,7 @@ while serverOn:
 
         except:
             print "accept timeout"
-            time.sleep(1)
+            time.sleep(10)
             # TODO remove break when debug finished
             # break
             pass
