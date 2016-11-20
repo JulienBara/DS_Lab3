13185e5492905ef578d8c3d88d74a395b09cda7e
JulienBara <bara.julien@yahoo.fr>
2016-11-20 00:04:45 +0000

    Revert "rm \n"
    This reverts commit a38a08838ff8b701fa9f30e2fa71b8a82e568bf3.

--- helper.py
+++ helper.py
@@ -35,7 +35,7 @@ def leaving(conn, chatroom_id, join_id, client_name):
     global joins
 
     s = "LEFT_CHATROOM:" + chatroom_id + "\n" \
-        + "JOIN_ID:" + join_id + ""
+        + "JOIN_ID:" + join_id + "\n"
     sendingMessageToCo(conn, s)
 
     chatroom = findOrDefaultChatroomById(int(chatroom_id), chatrooms)
